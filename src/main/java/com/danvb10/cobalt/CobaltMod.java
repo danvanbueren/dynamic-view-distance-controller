161705e9982c023cdbec6b47edfd88516d441654
Dan <danvb10@gmail.com>
2021-03-18 07:47:29 -0700

    Mod is now functioning without crashing the server, woo hoo

---
+++ src/main/java/com/danvb10/cobalt/CobaltMod.java
@@ -0,0 +1,11 @@
+package com.danvb10.cobalt;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CobaltMod implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Cobalt has been initialized");
+	}
+}
